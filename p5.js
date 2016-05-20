ba426c79d3de78143accdac9d3ddb43843f65dd8
narchontis <narchontis@gmail.com>
2016-05-19 22:04:48 -0400

    More stuff

--- p5.js
+++ p5.js
@@ -1,8 +1,14 @@
 function setup() {
-  // put setup code here
+   createCanvas(480, 120);
+   background(204);
+   print("Nick");
 }
 
 function draw() {
-  background(204);
-  ellipse(50, 50, 80, 80);
+  if (mouseIsPressed){
+      fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
