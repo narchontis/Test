2fe2c305e8bf8944cba5c5cf5b6c45c85cb7e07e
narchontis <narchontis@gmail.com>
2016-05-19 22:41:39 -0400

    Adding bubbles

--- p5.js
+++ p5.js
@@ -1,14 +1,24 @@
+var bubbles = [];
+
 function setup() {
    createCanvas(480, 120);
-   background(204);
-   print("Nick");
+   for (var i = 0; i < 10; i++) {
+     var x = random(width);
+     var y = random(height);
+     bubbles.push(new Bubble(x,y));
+   }
+   print("Nick is having some fun with p5 oh yeah.");
 }
 
 function draw() {
+  background(0);
+  for (var i = 0; i < bubbles.length; i++) {
+    bubbles[i].move();
+    bubbles[i].display();
+  }
+
   if (mouseIsPressed){
       fill(0);
-  } else {
-    fill(255);
+    ellipse(mouseX, mouseY, 80, 80);
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
