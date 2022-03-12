d562e3684fe31d25eda755ac8682beba8ed99f77
narchontis <narchontis@gmail.com>
2022-03-12 12:05:24 -0500

    Updating eslint file env.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
 };
